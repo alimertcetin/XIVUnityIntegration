6238c8279170c0e70908e0c228900613d4ab6b38
alimertcetin <mrtmert54@gmail.com>
2025-11-23 04:08:40 +0300

    feat(FollowCurveAndRotationVec3Tween): Use extension function to convert Vec3

--- XIVUnityIntegration/TweenSystem/TransformTweens/FollowCurveAndRotationVec3Tween.cs
+++ XIVUnityIntegration/TweenSystem/TransformTweens/FollowCurveAndRotationVec3Tween.cs
@@ -2,6 +2,7 @@
 using XIV.Core.DataStructures;
 using XIV.Core.TweenSystem.Drivers;
 using XIV.Core.XIVMath;
+using XIVUnityEngineIntegration.Extensions;
 
 namespace XIV.Core.TweenSystem.TransformTweens
 {
@@ -12,8 +13,8 @@ namespace XIV.Core.TweenSystem.TransformTweens
             // var point = BezierMath.GetPoint(startValue[0], startValue[1], startValue[2], startValue[3], easedTime);
             // component.position = point.ToVector3();
             var curveData = BezierMath.GetCurveData(startValue, easedTime);
-            component.position = curveData.point;
-            component.rotation = Quaternion.LookRotation(curveData.forward, curveData.normal);
+            component.position = curveData.point.ToVector3();
+            component.rotation = Quaternion.LookRotation(curveData.forward.ToVector3(), curveData.normal.ToVector3());
         }
     }
 }
