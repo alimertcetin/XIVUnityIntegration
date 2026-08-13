64afdd38742c1253ebb8311256ac8bda4a85ff75
alimertcetin <mrtmert54@gmail.com>
2026-08-13 16:42:55 +0300

    feat: Add XIVQuaternionExtensions

---
+++ XIVUnityIntegration/Extensions/XIVQuaternionExtension.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using XIV.Core.DataStructures;
+
+namespace XIVUnityEngineIntegration.Extensions
+{
+    public static class XIVQuaternionExtension
+    {
+        public static Quaternion ToQuaternion(this XIVQuaternion q)
+        {
+            return new Quaternion(q.x, q.y, q.z, q.w);
+        }
+    }
+}
